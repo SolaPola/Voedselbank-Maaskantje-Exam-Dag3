cc1dd4fc0396b8a5652896c8cbfb6c65350cc057
mohamed-azzouz1 <339078@student.mboutrecht.nl>
2025-07-01 14:57:41 +0200

    commit 15

--- resources/views/FoodPackages/volunteer/family-packages.blade.php
+++ resources/views/FoodPackages/volunteer/family-packages.blade.php
@@ -56,7 +56,7 @@
                                 </td>
                                 <td class="px-4 py-2 border-b">{{ $package->product_count }}</td>
                                 <td class="px-4 py-2 border-b text-center">
-                                    <a href="{{ route('food-packages.edit', $package->id) }}" class="text-blue-600">
+                                    <a href="{{ route('volunteer.food-packages.edit', $package->id) }}" class="text-blue-600">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                         </svg>
