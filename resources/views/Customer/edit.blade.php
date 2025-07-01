86d39613a78268ceeee71ad2f25e5350c1d0bf03
Bilagio <338828@student.mboutrecht.nl>
2025-07-01 15:24:15 +0200

    added comments

--- resources/views/Customer/edit.blade.php
+++ resources/views/Customer/edit.blade.php
@@ -231,7 +231,7 @@
                                     {{ __('Wijzig Klant Details') }}
                                 </button>
                             </div>
-                            
+                            {{-- back button --}}
                             <div class="flex space-x-3">
                                 <a href="{{ route('customers.index') }}" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                                     {{ __('terug') }}
