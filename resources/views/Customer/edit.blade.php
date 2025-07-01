37f261d59117471a865ddcf83782824d1d601dbd
Bilagio <338828@student.mboutrecht.nl>
2025-07-01 15:21:56 +0200

    removed home button

--- resources/views/Customer/edit.blade.php
+++ resources/views/Customer/edit.blade.php
@@ -236,9 +236,7 @@
                                 <a href="{{ route('customers.index') }}" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                                     {{ __('terug') }}
                                 </a>
-                                <a href="{{ route('dashboard') }}" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                                    {{ __('home') }}
-                                </a>
+                                
                             </div>
                         </div>
                     </form>
