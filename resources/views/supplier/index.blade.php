c3205ce5357c7373cc7803c21ceb97d4a4c63502
studentlucas <338362@student.mboutrecht.nl>
2025-07-01 13:45:15 +0200

    verbetering

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -36,14 +36,14 @@
                     @php $contact = $supplier->contacts->first(); @endphp
                     <tr>
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $supplier->name }}</td>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $contact?->name ?? 'No contact' }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $supplier?->contact_person}}</td>
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $contact?->email ?? '' }}</td>
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $contact?->mobile ?? '' }}</td>
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $supplier->supplier_number }}</td>
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $supplier->supplier_type }}</td>
                         <td class="px-6 py-4 whitespace-nowrap">
-                            <a href="{{ route('manager.suppliers.products', $supplier->id) }}">
-                                <i class="bi bi-clipboard"></i>
+                            <a href="{{ route('manager.suppliers.products', $supplier->id) }}" class="inline-flex items-center px-3 py-1 bg-blue-500 text-white text-xs font-semibold rounded hover:bg-blue-700 transition">
+                                <i class="bi bi-clipboard mr-1"></i> Show
                             </a>
                         </td>
                     </tr>
